8250ddf295eea593fc9bad1a9c351e524cfd8149
Plusholic <97847752+Plusholic@users.noreply.github.com>
2023-01-13 23:54:40 +0900

    Add files via upload

--- 카드 역배치.py
+++ 카드 역배치.py
@@ -1,12 +1,27 @@
 import sys
 sys.stdin = open("algorithm/input.txt", "rt")
 
-card = [i+1 for i in range(20)]
+
+# 1부터 20까지 쓰인 카드
+# [5 10] -> 1 2 3 4 10 9 8 7 6 5 11 12 ~
+
+cards = [i+1 for i in range(20)]
+# print(cards)
+# print(cards[1::-1])
 for _ in range(10):
-    a, b = list(map(int, input().split()))
-    card[a-1:b] = cardb-1:a-2:-1]
-    print(card)
-    # print(card[:a-1])
-# a, b = 5, 10
-# card = card[:a-1] + card[b:a-2:-1] + card[b+1:]
-print(card)
+    a, b = map(int, input().split())
+    if a != 1:
+        cards[a-1:b] = cards[b-1:a-2:-1]
+        print(cards)
+
+    elif a == 1:
+        cards[a-1:b] = cards[b-1::-1]
+        print(cards)
+        
+    # 5, 10
+    # 4:10
+    # 9:3
+    # print(a, b)
+    # for i in range(a, b):
+    #     cards[i-1] = cards[b+a-(i-1)]
+    #     print(cards)
